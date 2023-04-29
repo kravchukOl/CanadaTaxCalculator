8f477f268d132a2ac6257b37212bc2813418ce0a
Oleksii <longision@gmail.com>
2023-04-29 09:37:19 -0400

    getFederalTaxAdded

--- app/src/main/java/com/oleksiikravchuk/canadataxcalculator/IncomeTax.kt
+++ app/src/main/java/com/oleksiikravchuk/canadataxcalculator/IncomeTax.kt
@@ -1,7 +1,32 @@
 package com.oleksiikravchuk.canadataxcalculator
 
-class IncomeTax(private var province: Province) {
+class IncomeTax(private val province: Province, private val annualIncome: Double) {
+
+    private val FederalTaxBrakets2023 = arrayOf(
+        Pair(0, 0.15),
+        Pair(53359, 0.205),
+        Pair(106717, 0.26),
+        Pair(165430, 0.29),
+        Pair(235675, 0.33)
+    )
 
 
+    public fun getFederalTax(): Double {
+        var taxValue = 0.0;
+
+        for (i in 1..FederalTaxBrakets2023.size) {
+            if (i == FederalTaxBrakets2023.size && annualIncome > FederalTaxBrakets2023[i - 1].first) {
+                taxValue += (annualIncome - FederalTaxBrakets2023[i - 1].first) * FederalTaxBrakets2023[i - 1].second
+                break
+            }
+            if (annualIncome <= FederalTaxBrakets2023[i].first) {
+                taxValue += (annualIncome - FederalTaxBrakets2023[i - 1].first) * FederalTaxBrakets2023[i - 1].second
+                break;
+            } else {
+                taxValue += (FederalTaxBrakets2023[i].first - FederalTaxBrakets2023[i - 1].first) * FederalTaxBrakets2023[i - 1].second
+            }
+        }
+        return taxValue
+    }
 
 }
--- app/src/main/java/com/oleksiikravchuk/canadataxcalculator/income/IncomeTaxFragment.kt
+++ app/src/main/java/com/oleksiikravchuk/canadataxcalculator/income/IncomeTaxFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.oleksiikravchuk.canadataxcalculator.IncomeTax
 import com.oleksiikravchuk.canadataxcalculator.Province
 import com.oleksiikravchuk.canadataxcalculator.R
 import com.oleksiikravchuk.canadataxcalculator.adapters.ProvinceArrayAdapter
@@ -13,6 +15,7 @@ import com.oleksiikravchuk.canadataxcalculator.databinding.FragmentIncomeTaxBind
 class IncomeTaxFragment : Fragment() {
 
     private lateinit var binding: FragmentIncomeTaxBinding
+    private lateinit var incomeTax : IncomeTax
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,34 +29,45 @@ class IncomeTaxFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUI()
-
     }
 
     private fun setUI() {
         setSpinnerAdapter()
+        binding.buttonCalculateTaxes.setOnClickListener { calculateTaxes() }
+    }
+
+
+    private fun calculateTaxes() {
+
+        if(binding.editTextAnnualIncome.text.isNullOrEmpty()) {
+            Toast.makeText(context, "Enter Annual Income", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        incomeTax = IncomeTax( Province("Alberta", 0),
+            binding.editTextAnnualIncome.text.toString().toDouble())
+
+        binding.textViewFederal1Tax.text = incomeTax.getFederalTax().toString()
     }
 
     private fun setSpinnerAdapter() {
         val provincesArray = arrayOf(
-            Province( "Alberta", R.drawable.flag_of_alberta),
-            Province( "British Columbia", R.drawable.flag_of_british_columbia),
-            Province( "Manitoba", R.drawable.flag_of_manitoba),
-            Province( "New Brunswick", R.drawable.flag_of_new_brunswick),
-            Province( "Newfoundland and Labrador", R.drawable.flag_of_newfoundland_and_labrador),
-            Province( "Northwest Territories", R.drawable.flag_of_the_northwest_territories),
-            Province( "Nova Scotia", R.drawable.flag_of_nova_scotia),
-            Province( "Nunavut", R.drawable.flag_of_nunavut),
-            Province( "Ontario", R.drawable.flag_of_ontario),
-            Province( "Prince Edward Island", R.drawable.flag_of_prince_edward_island),
-            Province( "Quebec", R.drawable.flag_of_quebec),
-            Province( "Saskatchewan", R.drawable.flag_of_saskatchewan),
-            Province( "Yukon", R.drawable.flag_of_yukon),
+            Province("Alberta", R.drawable.flag_of_alberta),
+            Province("British Columbia", R.drawable.flag_of_british_columbia),
+            Province("Manitoba", R.drawable.flag_of_manitoba),
+            Province("New Brunswick", R.drawable.flag_of_new_brunswick),
+            Province("Newfoundland and Labrador", R.drawable.flag_of_newfoundland_and_labrador),
+            Province("Northwest Territories", R.drawable.flag_of_the_northwest_territories),
+            Province("Nova Scotia", R.drawable.flag_of_nova_scotia),
+            Province("Nunavut", R.drawable.flag_of_nunavut),
+            Province("Ontario", R.drawable.flag_of_ontario),
+            Province("Prince Edward Island", R.drawable.flag_of_prince_edward_island),
+            Province("Quebec", R.drawable.flag_of_quebec),
+            Province("Saskatchewan", R.drawable.flag_of_saskatchewan),
+            Province("Yukon", R.drawable.flag_of_yukon),
         )
-
         binding.spinnerProvinces.adapter = ProvinceArrayAdapter(provincesArray)
     }
-
-
 
 
 }
